b90667bb36b9a00ba701c94144fff90ca5e1e87b
ASMAXI <108028037+ASMAXI@users.noreply.github.com>
2024-10-06 21:21:13 +0300

    Initial Commit

---
+++ CustomHLSMediaPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomHLSMediaPlayer
+//
+//  Created by  Max on 06.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomHLSMediaPlayerTests/CustomHLSMediaPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHLSMediaPlayerTests.swift
+//  CustomHLSMediaPlayerTests
+//
+//  Created by  Max on 06.10.2024.
+//
+
+import Testing
+@testable import CustomHLSMediaPlayer
+
+struct CustomHLSMediaPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
